4abd2204e639aa13f6b319c6a452fdbee25a5452
Stepanav <vsa83@yandex.ru>
2022-01-05 16:25:59 +0300

    Создан scroll

---
+++ app/src/main/java/ru/gb/course1/l6_recycler/EmployeeEntity.java
@@ -0,0 +1,110 @@
+package ru.gb.course1.l6_recycler;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class EmployeeEntity implements Parcelable {
+    private String id;
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private int salary;
+    private String position;
+
+    public EmployeeEntity() {
+
+    }
+
+    public EmployeeEntity(String id, String name, String surname, String phoneNumber, int salary, String position) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.position = position;
+    }
+
+    protected EmployeeEntity(Parcel in) {
+        id = in.readString();
+        name = in.readString();
+        surname = in.readString();
+        phoneNumber = in.readString();
+        salary = in.readInt();
+        position = in.readString();
+    }
+
+    public static final Creator<EmployeeEntity> CREATOR = new Creator<EmployeeEntity>() {
+        @Override
+        public EmployeeEntity createFromParcel(Parcel in) {
+            return new EmployeeEntity(in);
+        }
+
+        @Override
+        public EmployeeEntity[] newArray(int size) {
+            return new EmployeeEntity[size];
+        }
+    };
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(name);
+        dest.writeString(surname);
+        dest.writeString(phoneNumber);
+        dest.writeInt(salary);
+        dest.writeString(position);
+    }
+}
--- app/src/main/java/ru/gb/course1/l6_recycler/MainActivity.java
+++ app/src/main/java/ru/gb/course1/l6_recycler/MainActivity.java
@@ -3,12 +3,99 @@ package ru.gb.course1.l6_recycler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout listLinearLayout;
+
+    private final ArrayList<EmployeeEntity> employeeList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        employeeList.addAll(CreateDummyEmployeesData());
+
+        listLinearLayout = findViewById(R.id.list_linear_layout);
+
+        Button button = new Button(this);
+        button.setText("Привет");
+        listLinearLayout.addView(button);
+
+
+        for (EmployeeEntity employeeEntity : employeeList) {
+            View employeeItemView = getLayoutInflater().inflate(R.layout.item_employee, listLinearLayout, false);
+            ((TextView) employeeItemView.findViewById(R.id.name_text_view)).setText(employeeEntity.getName());
+            ((TextView) employeeItemView.findViewById(R.id.surname_text_view)).setText(employeeEntity.getSurname());
+            ((TextView) employeeItemView.findViewById(R.id.position_text_view)).setText(employeeEntity.getPosition());
+            listLinearLayout.addView(employeeItemView);
+        }
+
+    }
+
+    private static ArrayList<EmployeeEntity> CreateDummyEmployeesData() {
+        final ArrayList<EmployeeEntity> employeeEntities = new ArrayList<>();
+        employeeEntities.add(new EmployeeEntity(
+                "0",
+                "Иван",
+                "Иванов",
+                "88005553535",
+                500_000,
+                "Android developer"
+        ));
+
+        employeeEntities.add(new EmployeeEntity(
+                "1",
+                "Пётр",
+                "Петров",
+                "88005553535",
+                400_000,
+                "iOS developer"
+        ));
+
+        employeeEntities.add(new EmployeeEntity(
+                "2",
+                "Сидр",
+                "Сидоров",
+                "88005553535",
+                1_000_000,
+                "Самый главный директор"
+        ));
+        employeeEntities.add(new EmployeeEntity(
+                "3",
+                "Мария",
+                "Иванова",
+                "88005553535",
+                500_000,
+                "PHP developer"
+        ));
+        employeeEntities.add(new EmployeeEntity(
+                "4",
+                "Юрий",
+                "Юрьев",
+                "88005553535",
+                100_000,
+                "Junior developer"
+        ));
+
+        employeeEntities.add(new EmployeeEntity(
+                "5",
+                "Игнат",
+                "Игнатьев",
+                "88005553535",
+                300_000,
+                "Designer"
+        ));
+
+        return employeeEntities;
     }
 }
